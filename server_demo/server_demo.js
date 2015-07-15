bdee465d34ae37877e58a1717721a6eca04e8449
Corey600 <fcx600@163.com>
2015-07-15 11:33:14 +0000

    初始化

---
+++ server_demo/server_demo.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(reg, res){
+    if('GET' == reg.method
+        && '/images' == reg.url.substr(0, 7)
+        && '.jpg' == reg.url.substr(-4)){
+        fs.stat(__dirname + reg.url, function(err, stat){
+            if(err || !stat.isFile()){
+                res.writeHead(404);
+                res.end('not found1!');
+                return;
+            }
+            serve(__dirname + reg.url, 'application/jpg');
+        });
+    }else if('GET' == reg.method
+        && '/' == reg.url){
+        serve(__dirname + '/index.html', 'text/html');
+    }else{
+        res.writeHead(404);
+        res.end('not found2!');
+    }
+
+    function serve(path, type){
+        res.writeHead(200, {'Content-Type': type});
+        fs.createReadStream(path).pipe(res);
+    }
+});
+
+server.listen(3000);
