8e389ff91739997724e2e571b3d92c5720263cc6
Corey600 <fcx600@163.com>
2015-07-15 14:39:30 +0000

    修改file_explorer

--- file_explorer/index.js
+++ file_explorer/index.js
@@ -1,27 +1,30 @@
 /**
- * Created by feichenxi on 2015/5/28.
+ * Created by fcx on 2015/5/30.
  */
+
 var fs = require('fs');
 var stdin = process.stdin;
 var stdout = process.stdout;
+var stats = [];
 
-fs.readdir(process.cwd(), function(err, files){
+fs.readdir(process.cwd(), function (err, files) {
     console.log('');
     if (!files.length) {
-        return console.log('    \033[31mNo files to show! \033[39m\n');
+        return console.log('    \033[31m No files to show!\033[39m');
     }
     console.log('    Select which file or directory you want to see\n');
 
     function file(i) {
         var filename = files[i];
         fs.stat(__dirname + '/' + filename, function(err, stat) {
-            if(stat.isDirectory()) {
-                console.log('    ' + i + '    \33[36m' + filename + '/\033[39m');
+            stats[i] = stat;
+            if (stat.isDirectory()) {
+                console.log('    ' + i + '    \033[36m' + filename + '/\033[39m');
             } else {
-                console.log('    ' + i + '    \33[36m' + filename + '\033[39m');
+                console.log('    ' + i + '    \033[90m' + filename + '\033[39m');
             }
             i++;
-            if (i >= files.length) {
+            if (i == files.length) {
                 read();
             } else {
                 file(i);
@@ -39,20 +42,32 @@ fs.readdir(process.cwd(), function(err, files){
 
     function option(data) {
         var filename = files[Number(data)];
-
-        if(!filename) {
+        if (!filename) {
             stdout.write('    \033[31mEnter your choice: \033[39m');
         } else {
             stdin.pause();
-            fs.readFile(__dirname + '/' + filename, 'utf8', function(err, data) {
-                console.log('');
-                console.log('\033[90m' + data.replace(/(.*)/g, '    $1') + '\033[39m');
-            });
+            if (stats[Number(data)].isDirectory()) {
+                fs.readdir(__dirname + '/' + filename, function(err, files) {
+                    console.log('');
+                    console.log('    (' + files.length + ' files)');
+                    files.forEach(function(file) {
+                        console.log('    -  ' + file);
+                    });
+                    console.log('');
+                    stdout.write('    \033[33mEnter your choice: \033[39m');
+                    stdin.resume();
+                });
+            } else {
+                fs.readFile(__dirname + '/' + filename, 'utf8', function(err, data) {
+                    console.log('');
+                    console.log('\033[90m' + data.replace(/(.*)/g, '    $1') + '\033[39m');
+                    console.log('');
+                    stdout.write('    \033[33mEnter your choice: \033[39m');
+                    stdin.resume();
+                });
+            }
         }
     }
 
     file(0);
 });
-
-
-
