f43a2ff1cbc692634bbe0073863b5d946e2f4771
Corey600 <fcx600@163.com>
2015-07-16 13:51:46 +0000

    my_website 增加文件处理

--- my_website/index.js
+++ my_website/index.js
@@ -2,6 +2,7 @@
  * Created by feichenxi on 2015/7/15.
  */
 
+var fs = require('fs');
 var connect = require('connect');
 var time = require('./lib/request_time');
 
@@ -18,9 +19,14 @@ server.use(connect.logger('short'));
 server.use(time({ time: 500 }));
 
 /**
+ * body 解析中间件
+ */
+server.use(connect.bodyParser({ keepExtensions: true, uploadDir: './files' }));
+
+/**
  * 托管静态资源
  */
-server.use(connect.static(__dirname + '/website'));
+server.use(connect.static('website'));
 
 /**
  * 快速响应
@@ -43,6 +49,32 @@ server.use(function(req, res, next){
             res.writeHead(200);
             res.end('Slow!');
         }, 1000);
+    }else{
+        next();
+    }
+});
+
+/**
+ * 文件处理
+ */
+server.use(function(req, res, next){
+    if('POST' == req.method && req.files){
+        //console.log(req.files); // 书上有误，不是req.body.file而是req.files
+        for(var file in req.files){
+            if(req.files.hasOwnProperty(file)){
+                fs.readFile(file.path, 'utf8', function(err, data){
+                    if(err){
+                        res.writeHead(500);
+                        res.end('Error');
+                        return;
+                    }
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end([
+                        '<h3>File: '+file
+                    ].join(''));
+                });
+            }
+        }
     }else{
         next();
     }
