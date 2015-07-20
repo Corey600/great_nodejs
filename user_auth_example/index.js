a0b4d207434eda98ffba41d19366e74b8c24a1df
Corey600 <fcx600@163.com>
2015-07-20 20:00:28 +0800

    new demo: user_auth_example

---
+++ user_auth_example/index.js
@@ -0,0 +1,53 @@
+/**
+ * Created by feichenxi on 2015/7/20.
+ */
+
+/**
+ * 模块依赖
+ */
+var express = require('express');
+var mongodb = require('mongodb');
+
+/**
+ * 构建引用程序
+ */
+var app = express.createServer();
+
+/**
+ * 中间件
+ */
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'my secret' }));
+
+/**
+ * 指定视图选项
+ */
+app.set('view engine', 'jade');
+app.set('view options', { layout: true });
+
+/**
+ * 默认路由
+ */
+app.get('/', function(req, res){
+    res.render('index', { authenticated: false });
+});
+
+/**
+ * 默认路由
+ */
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+/**
+ * 默认路由
+ */
+app.get('/signup', function(req, res){
+    res.render('signup');
+});
+
+/**
+ * 监听
+ */
+app.listen(3000);
